821ce39a68e1fd6767745a84a2c2902766eabd7c
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-01-25 19:50:16 +0530

    added images

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import img1 from './images/img1.svg';
+import img2 from './images/img2.svg';
+import logo from './images/logo.svg';
+import profile from './images/profile.svg';
+import tonkean from './images/tonkean.svg';
+import bigspring from './images/bigspring.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt='logo' />
+      <img src={img1} alt='logo' />
+      <img src={img2} alt='logo' />
+      <img src={profile} alt='logo' />
+      <img src={tonkean} alt='logo' />
+      <img src={bigspring} alt='logo' />
+     hiii
+    </div>
+  );
+}
+
+export default App;

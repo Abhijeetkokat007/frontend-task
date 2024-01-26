c1b1f72f35d4df78da7cd4d57bc29b5f4f985711
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-01-26 06:18:24 +0530

    added navbar and poster

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import bigspring from './images/bigspring.svg';
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
 import Header from './component/Header';
+import Postermain from './component/Poster/Postermain';
 
 function App() {
   return (
-    
+    <div >
+      {/* <img src={logo} alt='logo' />
+      <img src={img1} alt='logo' />
+      <img src={img2} alt='logo' />
+      <img src={profile} alt='logo' />
+      <img src={tonkean} alt='logo' />
+      <img src={bigspring} alt='logo' />
+     hiii */}
+
+
+
+{/* <Header/> */} 
+
+<Postermain/>
+
+    </div>
   );
 }
 
---
+++ src/component/Poster/Postermain.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import img1 from './../../images/img1.svg';
+import starimg from './../../images/starimg.svg';
+import './Postermain.css'
+import Navbar from '../Navbar/Navbar';
+
+function Postermain() {
+  return (
+    <div>
+       <section className='mt-3'>
+       <Navbar/> 
+       
+<div className='section-bg'>
+
+<div className='tri'>
+<div className='mt-5 sub-container-app d-flex justify-content-evenly align-items-center flex-wrap-reverse '>
+           <div className=' text-div'>
+           <button className='btn-post-version'> <img src={starimg} alt='logo' />  v3.1 released. Learn more </button>
+               <h1 className='post-hedding mt-4'>Your data with real-time analytics</h1>
+               <p className='small-text-post mt-3 mb-3'>Harness the potential of Big Data Analytics & Cloud Services and become a data-driven organization with Needle tail</p>
+               <button className='btn-post-start'> Start Free Trial </button>
+               <button className='btn-post-trial ms-5'> Learn More </button>
+           </div>
+       <img src={img1} alt='logo' /> 
+       
+       </div>
+       </div>
+</div>
+    
+  
+ 
+</section>
+
+{/* <section >
+       
+
+       <div className='sub-container-app d-flex justify-content-evenly align-items-center pad'>
+           <div className='p-3'>
+           <button className='btn-post-version'> <img src={starimg} alt='logo' />  v3.1 released. Learn more </button>
+               <h1 className='post-hedding'>Your data with real-time analytics</h1>
+               <p className='small-text-post'>Harness the potential of Big Data Analytics & Cloud Services and become a data-driven organization with Needle tail</p>
+               <button className='btn-post-start'> Start Free Trial </button>
+               <button className='btn-post-trial'> Learn More </button>
+           </div>
+       <img src={img1} alt='logo' /> 
+       
+       </div>
+           
+         
+        
+       </section> */}
+    </div>
+  )
+}
+
+export default Postermain
